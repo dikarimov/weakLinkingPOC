6686f0e2e1c887c196a3020e5408189fd468311c
Dinar Karimov <dinark.ctr@altia.com>
2022-11-24 03:27:36 -0500

    Linux works

---
+++ lib.cpp
@@ -0,0 +1,5 @@
+#include "lib.hpp"
+
+void func(){
+    std::cout << "Default func was called!" << std::endl;
+}
---
+++ main_overrided.cpp
@@ -0,0 +1,9 @@
+#include "lib.hpp"
+
+void func(){
+    std::cout << "Overrided func was called!" << std::endl;
+}
+
+int main(){
+    func();
+}
